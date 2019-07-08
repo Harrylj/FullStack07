936d3ecd2e5e33c1b7e084e00a40d5b20f45711a
Harrylj <18200166593@163.com>
2019-07-08 18:17:24 +0800

    2019-07-08 18:17

---
+++ webpack01/src/a.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2019/7/8.
+ */
+module.exports = () => {
+    console.log('我是01a.js')
+}
---
+++ webpack01/src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2019/7/8.
+ */
+const hello = require('./a.js')
+hello()
